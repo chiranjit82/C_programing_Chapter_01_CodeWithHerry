8a545efc43f90c134078a024e988e1a2db906539
Chiranjit <chiranjitdas61199@gmail.com>
2022-05-16 12:12:52 +0530

    Final Commit

--- .vscode/04_practice.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    float principle = 1000, rateofInterest = 5, years = 1;
-    float SimpleInterest = (principle * rateofInterest * years)/100;
-    printf("The simple Interest is %f", SimpleInterest);
-    return 0;
-}
